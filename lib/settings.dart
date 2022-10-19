8cfc7a8bd4ea5dd19019a088785b360982ae5e8f
Yue-plus <Yue_plus@foxmail.com>
2022-10-20 02:25:23 +0800

    add settings 表单验证

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,4 +1,3 @@
-// import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -22,17 +21,20 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingState extends State<Settings> {
+  final GlobalKey _formKey = GlobalKey<FormState>();
+
   // https://flutter.cn/docs/cookbook/persistence/key-value
   late final SharedPreferences prefs;
 
   /// 认证主机
-  String authHost = '10.31.0.10:801';
+  late String authHost;
   final TextEditingController _authHostController = TextEditingController();
 
   /// 认证主机输入框
   TextFormField _authHostTextField() {
     return TextFormField(
       controller: _authHostController,
+      validator: (v) => v!.trim().isNotEmpty ? null : "认证主机不能为空！",
       decoration: const InputDecoration(
         labelText: '认证服务器',
         hintText: '[IP 地址 / 域名]:[端口号]',
@@ -42,14 +44,18 @@ class _SettingState extends State<Settings> {
   }
 
   /// 学号
-  String stuNum = '';
+  late String stuNum;
   final TextEditingController _stuNumController = TextEditingController();
+
+  /// 学号正则验证；学号应该为 9 位数字
+  final RegExp stuNumRegExp = RegExp(r'\d{8}[0-9]');
 
   /// 学号输入框
   TextFormField _stuNumTextFormField() {
     return TextFormField(
       autofocus: true,
       controller: _stuNumController,
+      validator: (v) => stuNumRegExp.hasMatch(v!.trim()) ? null : '学号应为 9 位数字！',
       keyboardType: TextInputType.number,
       decoration: const InputDecoration(labelText: '学号'),
       onChanged: (v) => setState(() => stuNum = v),
@@ -73,13 +79,14 @@ class _SettingState extends State<Settings> {
   }
 
   /// 密码
-  String password = '';
+  late String password;
   final TextEditingController _passwordController = TextEditingController();
 
   /// 密码输入框
   TextFormField _passwordFormField() {
     return TextFormField(
       controller: _passwordController,
+      validator: (v) => v!.trim().isNotEmpty ? null : "密码不能为空！",
       keyboardType: TextInputType.visiblePassword,
       obscureText: true,
       decoration: const InputDecoration(labelText: '密码'),
@@ -103,17 +110,25 @@ class _SettingState extends State<Settings> {
   }
 
   Future<void> _save() async {
-    prefs.setString('authHost', authHost);
-    prefs.setString('stuNum', stuNum);
+    prefs.setString('authHost', authHost.trim());
+    prefs.setString('stuNum', stuNum.trim());
     prefs.setString('isp', isp.toString());
-    prefs.setString('password', password);
+    prefs.setString('password', password.trim());
     Navigator.pop(context);
   }
 
   Future<void> _initSettings() async {
     prefs = await SharedPreferences.getInstance();
-    _authHostController.text = prefs.getString('authHost') ?? '10.31.0.10:801';
-    _stuNumController.text = prefs.getString('stuNum') ?? '';
+
+    // 初始化本类成员变量
+    authHost = prefs.getString('authHost') ?? '10.31.0.10:801';
+    stuNum = prefs.getString('stuNum') ?? '';
+    password = password = prefs.getString('password') ?? '';
+
+    // 初始化界面
+    _authHostController.text = authHost;
+    _stuNumController.text = stuNum;
+    _passwordController.text = password;
     switch (prefs.getString('isp')) {
       case 'ISP.unicom':
         setState(() => isp = ISP.unicom);
@@ -124,7 +139,6 @@ class _SettingState extends State<Settings> {
       default:
         setState(() => isp = ISP.telecom);
     }
-    _passwordController.text = password = prefs.getString('password') ?? '';
   }
 
   @override
@@ -141,22 +155,26 @@ class _SettingState extends State<Settings> {
         child: Container(
           width: 800,
           padding: const EdgeInsets.all(8),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              _authHostTextField(),
-              Row(
-                textBaseline: TextBaseline.ideographic,
-                crossAxisAlignment: CrossAxisAlignment.baseline,
-                children: [
-                  Expanded(child: _stuNumTextFormField()),
-                  const SizedBox(width: 8),
-                  _ispDropdownButton(),
-                ],
-              ),
-              _passwordFormField(),
-              _saveButton(),
-            ],
+          child: Form(
+            key: _formKey,
+            autovalidateMode: AutovalidateMode.always,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                _authHostTextField(),
+                Row(
+                  textBaseline: TextBaseline.ideographic,
+                  crossAxisAlignment: CrossAxisAlignment.baseline,
+                  children: [
+                    Expanded(child: _stuNumTextFormField()),
+                    const SizedBox(width: 8),
+                    _ispDropdownButton(),
+                  ],
+                ),
+                _passwordFormField(),
+                _saveButton(),
+              ],
+            ),
           ),
         ),
       ),
